33e7bd0e4c6c012a45ed32bf15f59c9ec688ea7e
Khaled Zakaria <khaled71612000@gmail.com>
2022-06-29 10:51:36 +0200

    first commit

---
+++ ObjectPool.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternTask03
+{
+    public class Particle
+    {
+       public int limit;
+       public double x, y, Xvel, Yvel;
+        public void start(double x, double y, double xvol, double yvol, int wall)
+        {
+            this.x = x;
+            this.y = y;
+            Xvel = xvol;
+            Yvel = yvol;
+            limit = wall;
+        }
+
+        public void move()
+        {
+            if (!inUse()) return;
+
+            limit -=1;
+            this.x += Xvel;
+            this.y += Yvel;
+        }
+
+      public  bool inUse()  {
+            return limit > 0; 
+        }
+    }
+
+
+    class ParticlePool
+    {
+        public Particle[] particles = new Particle[100];
+        public void create(double x, double y, double xvol, double yvol, int wall) {
+            for (int i = 0; i< 100; i++)
+            {
+                particles[i] = new Particle();
+                if (!particles[i].inUse())
+                {
+                    particles[i].start(x,y, xvol, yvol, wall);
+                    return;
+                }
+            }
+        }
+
+        public void moving()
+        {
+                particles[0].move();
+
+        }
+
+        public override string ToString()
+        {
+            return ($"Bullet pos on on X is {particles[0].x} and Bullet pos on Y is {particles[0].y}");
+            
+        }
+
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternTask03
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            ParticlePool bullet = new ParticlePool();
+            bullet.create(100, 100, 2 , 2 , 2000);
+            int i = 4100;
+            while (i > 0)
+            {
+                bullet.moving();
+                Console.WriteLine(bullet.ToString());
+                i--;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
